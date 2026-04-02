cfc5f029ba5a0c67b06164319c6f39e498230efd
Pranjal22100 <pranjal22100@iiitnr.edu.in>
2026-04-02 15:50:24 +0530

    Update use-socket.ts
    update use socket base url to remove check

--- client/src/hooks/use-socket.ts
+++ client/src/hooks/use-socket.ts
@@ -1,8 +1,7 @@
 import { io, Socket } from "socket.io-client";
 import { create } from "zustand";
 
-const BASE_URL =
-  import.meta.env.MODE === "development" ? import.meta.env.VITE_API_URL : "/";
+const BASE_URL = import.meta.env.VITE_API_URL; // always use it, same as axios fix
 
 interface SocketState {
   socket: Socket | null;
